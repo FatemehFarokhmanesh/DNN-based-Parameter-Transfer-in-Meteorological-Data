f9c06e774ad0af5009e36f8e2ec9c66d31bcf676
Fatemeh Farokhmanesh <fatemeh.farokhmanesh@tum.de>
2022-03-21 14:30:04 +0100

    Initial commit

--- configs/project_configs.py
+++ configs/project_configs.py
@@ -6,7 +6,7 @@ class ProjectConfigs:
     # directories
     HOME_DIR: str = os.environ['HOME']
     SCRIPT_PATH: str = './TrainingScript.py'
-    INTERPRETER_PATH: str = HOME_DIR + '/anaconda3/envs/wbd/bin/python'
+    INTERPRETER_PATH: str = HOME_DIR + '/anaconda3/envs/env_name/bin/python'
     QUEUE_PATH: str = './queue'
-    DATA_PATH: str = HOME_DIR + '/codes/numpy/32_64'
+    DATA_PATH: str = HOME_DIR + '/data_directory/numpy'
     EXPEREMENT_PATH: str = './script_experiment'
